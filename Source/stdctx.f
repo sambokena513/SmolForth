3a83766f4eea057ef838ef5d84bbe29f90c09e6f
Ofenbauer <sambokena@mail.com>
2026-08-22 15:04:57 +0200

    Made a few changes so that switching contexts is simpler.

--- Source/stdctx.f
+++ Source/stdctx.f
@@ -1,4 +1,4 @@
-( Dependencies: bootstrap.f, stdstring.f )
+( Dependencies: bootstrap.f )
 
 ( <stdctx.f> :; Execution contexts and the ability to switch between them, as
 well as various related utilities. )
@@ -7,6 +7,7 @@ well as various related utilities. )
 you'd do `CTX @d @d`, not `CTX @d`, or to get an offset into the third field,
 `CTX @d ctx.eSP_BASE FIELD` )
 HERE 32 ALLOT DWORD_T VARIABLE CTX CTX !d
+CTX @d CONSTANT MAIN_CTX ( 
 
 ( Note that these stack pointers and base pointers are image-relative, despite the fact that
 the dSP@ and rSP@ words use absolute pointers. )
@@ -19,7 +20,7 @@ HERE 4096 ALLOT 12 CONSTANT ctx.lSP_BASE CTX @d ctx.lSP_BASE FIELD !d
     While the base pointers of a context should always be up to date,
     these pointers are allowed to be out-of date in the interests of performance.
 
-    If you need the current state of these call UPDATE_CTX first, which will set all
+    If you need the current state of these call SAVE_CTX first, which will set all
     all of them to their current values.
 )
 
@@ -34,18 +35,19 @@ points to the *top item*. )
 
 (
     Actual eSP and lSP variables for use in the implementation of locals and exceptions,
-    these are the values copied into ctx.*SP by UPDATE_CTX
+    these are the values copied into ctx.*SP by SAVE_CTX
 )
 
 DWORD_T VARIABLE eSP CTX @d ctx.eSP FIELD @d eSP !d
 DWORD_T VARIABLE lSP CTX @d ctx.lSP FIELD @d lSP !d
 
-( UPDATE_CTX sets the current stack pointer fields in the current context to their values at the time of the call. )
-: UPDATE_CTX
+( SAVE_CTX sets the current stack pointer fields in the current context to their values at the time of the call.
+While applying an offset to the saved data stack and return stack pointers. )
+: SAVE_CTX ( dSP_offset rSP_offset -- )
+    rSP@ BASE SWAP - 8 + + CTX @d ctx.rSP FIELD !d
+    dSP@ BASE SWAP - -8 + - CTX @d ctx.dSP FIELD !d
     eSP @d CTX @d ctx.eSP FIELD !d
     lSP @d CTX @d ctx.lSP FIELD !d
-    dSP@ BASE SWAP - CTX @d ctx.dSP FIELD !d
-    rSP@ BASE SWAP - 8 + ( 8 + to skip the return address pushed by UPDATE_CTX itself being called. ) CTX @d ctx.rSP FIELD !d
 ;
 
 ( RUN_CTX sets the current stack pointers based on the fields in the current context at the time of the call. )
@@ -54,4 +56,12 @@ DWORD_T VARIABLE lSP CTX @d ctx.lSP FIELD @d lSP !d
     CTX @d ctx.lSP FIELD @d lSP !d
     CTX @d ctx.dSP FIELD @d BASE + dSP!
     CTX @d ctx.rSP FIELD @d BASE + rSP!
+;
+
+( SWITCH_CTX is essentially the atomic task-swapping primitive.
+It takes in a context, saves the current context such that execution resumes right after the call to SWITCH_CTX,
+which is where you should put your cleanup code. )
+: SWITCH_CTX ( ctx -- )
+   8 8 SAVE_CTX ( one slot offset for rSP because of the SWITCH_CTX call frame, and one for dSP because of the ctx argument. )
+   CTX !d RUN_CTX ( make the context switch )
 ;
