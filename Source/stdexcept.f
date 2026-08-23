72cec1dde1aea1168b4595ef6811a77598f3e1a9
Ofenbauer <sambokena@mail.com>
2026-08-23 18:50:28 +0200

    Exceptions work during testing now, next up I'll probably polish stdio and maybe the runtime to handle syscall failures a bit better.

--- Source/stdexcept.f
+++ Source/stdexcept.f
@@ -3,11 +3,29 @@
 ( <stdexcept.f> :; Exceptions and exception handling. Uses the exception stack from <stdctx.f>. )
 
 
-( A handler in the code below refers to a value on the exception stack consisting of an execution context, and an address to resume execution at. )
+( A handler in the code below refers to a value on the exception stack consisting of stack depths, and an address to resume execution at. )
+(
+    C-style reference for the handler struct:
+
+    typedef struct Handler {
+        i32 ip; // instruction pointer
+        i32 rSP;
+        i32 lSP;
+        i32 dSP;
+    } Handler; // 16 bytes in total
+)
+
+( Push, pop, and read a 32-bit value respectively from the exception stack. )
+: >E eSP @d !d eSP @d 4 + eSP !d ;
+: E> eSP @d -4 + eSP !d eSP @d @d ;
+: E@ eSP @d -4 + @d ;
 
 ( r | -E- handler | handler-addr -D- )
 : PUSH_HANDLER
-    TODO" PUSH_HANDLER should push an exception handler to the exception stack."
+    >E
+    rSP@ BASE SWAP - 8 + >E
+    lSP @d >E
+    dSP@ BASE SWAP - >E
 ;
 
 ( c | -C- try-sys )
@@ -31,7 +49,16 @@ CREATE ENDTRY ALIAS THEN IMMEDIATE
 
 ( r | handler -E- | exception -D- )
 : THROW
-    TODO" THROW should pop an exception handler from the exception stack, pop a value from the data stack,
-          and transfer control to the nearest handler while calling it with that exception value as an arg.
-          Additionally, if the exception stack would underflow, it should call PANIC."
+    ( if eSP_BASE + 16 is greater than eSP we know that a pop would underflow the stack. )
+    eSP @d CTX @d ctx.eSP_BASE FIELD @d 16 + > IF
+        r" Uncaught Exception: install an exception handler or exit the process,
+           Continuing may cause memory corruption." PANIC
+    THEN
+
+    ( we juggle the exception value through the exception stack so it isn't lost when we set dSP )
+    E> SWAP >E BASE + dSP! E>
+    E> lSP !d
+    E> DUP E>
+    BASE + SWAP !q ( write handler into the return address )
+    BASE + rSP! ( restore return stack, thus throwing us into the handler )
 ;
