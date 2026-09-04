63b482843b59f2c76eea2fa5d7682be6c29b732d
Ofenbauer <sambokena@mail.com>
2026-09-04 17:23:55 +0200

    Make PANIC take two args instead so that messages can be formatted more easily. By extension this makes ASSERT now also take an extra arg.

--- Source/stdassert.f
+++ Source/stdassert.f
@@ -23,23 +23,31 @@ stdassert defines its own minimal PRINTLN that calls sys_exit on failure. )
     STDASSERTF_PRINT STDASSERTF_NEWLINE STDASSERTF_PRINT
 ;
 
-( Print an error message, reset REPL state, and start a nested REPL.
+( Print an error message and template, reset REPL state, and start a nested REPL.
 Lets the user choose whether to kill the process or try to repair state
 and continue. )
-: PANIC r" [PANIC] " ABORT STDASSERTF_PRINTLN INTERPRET ;
+: PANIC r" [PANIC] " ABORT STDASSERTF_PRINTLN STDASSERTF_PRINTLN INTERPRET ;
 
-( Panic if a boolean is false with the message "ASSERT FAILURE".
-Used for enforcing invariants, ideally only use this when debugging so that
-the condition isn't always checked at runtime in prod. )
-: ASSERT ~ IF r" ASSERT FAILURE" PANIC THEN ;
+( Panic if a condition is false with the message template "ASSERT FAILURE" )
+: ASSERT ( cond msg -- )
+    SWAP IF
+        POP
+    ELSE
+        r" ASSERT FAILURE" PANIC
+    THEN
+;
 
 ( Panic with a custom error message, used for when you have a function
 fully or partially not implemented, but you still want it to be callable,
 essentially this lets you act out the function's role manually,
 and then EXIT back to the caller without them noticing anything amiss. )
+: __TODO
+    r" TODO" PANIC
+;
+
 : TODO"
     COMPILE r" ( " ( )
-    [ CLEAR WORD PANIC FIND ] LITERAL ECR32
+    [ CLEAR WORD __TODO FIND ] LITERAL ECR32
 ; IMMEDIATE
 
 ( similar to PANIC, but with slightly different output,
